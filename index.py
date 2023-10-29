40d73f9af57442064df14d76c94da8a078c55c54
Luis <luis.riosbarrera@gmail.com>
2023-10-28 22:40:12 -0500

    Agregar titulo, textos y entradas para dicho texto

--- index.py
+++ index.py
@@ -3,5 +3,16 @@ from tkinter import messagebox
 
 ventana= Tk()
 ventana.title("Nuevo Producto")
+ventana.config(bg="#454546")
+
+#Titulo "NUEVO PRODUCTO"
+titulo_nuevo_producto=Label(ventana, text= "Nuevo producto", font="Arial, 35", bg="#454546", fg="#8ae0db")
+titulo_nuevo_producto.place(x=725, y=30, anchor=CENTER)
+
+#Labels and inputs
+lbl_nombre_producto=Label(ventana, text="Nombre del Producto: ", font="Arial, 15", bg="#454546", fg="#8ae0db")
+lbl_nombre_producto.place(x=15, y=70)
+entry_nombre_producto=Entry(ventana)
+entry_nombre_producto.place(x=214, y=75)
 
 ventana.mainloop()
