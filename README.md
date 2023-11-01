f4c907f0d3861fcf2379feb5601addae17f6bd65
Luis <luis.riosbarrera@gmail.com>
2023-10-31 22:23:36 -0500

    Agregue cambios en README y la cantidad del producto

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-REGISTRO DE PRODUCTOS
-<<<<<<< HEAD
-Hola
-=======
-
-
-Que tal?
->>>>>>> 1b93cc9cd2cfe76899754dea6c0b9ba9a2323d55
+Registro de Productos
--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 
 ventana= Tk()
 ventana.title("Nuevo Producto")
+ventana.geometry("1300x599")
 ventana.config(bg="#454546")
 
 #Titulo "NUEVO PRODUCTO"
@@ -10,9 +11,13 @@ titulo_nuevo_producto=Label(ventana, text= "Nuevo producto", font="Arial, 35", b
 titulo_nuevo_producto.place(x=725, y=30, anchor=CENTER)
 
 #Labels and inputs
+#PRODUCTO
 lbl_nombre_producto=Label(ventana, text="Nombre del Producto: ", font="Arial, 15", bg="#454546", fg="#8ae0db")
 lbl_nombre_producto.place(x=15, y=70)
 entry_nombre_producto=Entry(ventana)
 entry_nombre_producto.place(x=214, y=75)
+
+#CANTIDAD
+lbl_cantidad=Label(ventana, text="Cantidad", font="Arial, 15", bg="#454546", fg="#8ae0db")
 
 ventana.mainloop()
